9cb0d91527180c998c611f6b3b66f04845048654
Zack338 <zachary.rouch@student.swic.edu>
2025-11-04 16:36:30 -0600

    Rename products to items and update constant.js

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
-import products from "./db.js";
+import items from "./db.js";
 import { parsePrice } from "./lib.js";
 
-export const HIGHEST_PRICE = Math.max(
-  ...products.map((p) => parsePrice(p.price))
+export const HIGHEST_ITEM_COST = Math.max(
+  ...items.map((item) => parsePrice(item.price))
 );
