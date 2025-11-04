6eca2d962106ee96d2da2d40535e34bb3e688232
Zack338 <zachary.rouch@student.swic.edu>
2025-11-04 16:09:28 -0600

    Rename MAX_PRODUCT_PRICE to HIGHEST_PRICE

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 import products from "./db.js";
 import { parsePrice } from "./lib.js";
 
-export const MAX_PRODUCT_PRICE = Math.max(
+export const HIGHEST_PRICE = Math.max(
   ...products.map((p) => parsePrice(p.price))
 );
